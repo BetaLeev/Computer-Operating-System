7aa122e2d7a8c9bc80df192eafe45b833bdbf2ac
v <a@tok7.com>
2021-01-16 23:29:24 +0800

    :art: 新增内存管理以及目录结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,6 +22,15 @@ module.exports = {
                 //   '/'
                 // ]
               },
+              {
+                title: '内存管理',   // 必要的
+                path: '/memory-manage/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                // children: [
+                //   '/'
+                // ]
+              },
         ]
       }
   }
